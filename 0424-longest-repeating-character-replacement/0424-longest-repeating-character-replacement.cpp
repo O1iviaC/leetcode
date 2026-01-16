d485be1fb750e1cd02989548d3eaa00214efd0ee
Olivia Chan <115821929+O1iviaC@users.noreply.github.com>
2026-01-16 14:48:36 -0500

    Time: 0 ms (100%), Space: 10.9 MB (39.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        map<char, int> seen;
-        vector<char> str;
+        int count[26] = {0};
+        int left = 0;
         int maxFreq = 0;
-        char maxC;
-        int i = 0;
-        while(i < s.length()){
-            str.push_back(s[i]);
-            seen[s[i]] = seen[s[i]] + 1;
-            if(seen[s[i]] > maxFreq){
-                 maxFreq = seen[s[i]];
-                 maxC = s[i];
+        int best = 0;
+
+        for (int right = 0; right < (int)s.size(); right++) {
+            int idx = s[right] - 'A';
+            count[idx]++;
+            if (count[idx] > maxFreq) maxFreq = count[idx];
+
+            while ((right - left + 1) - maxFreq > k) {
+                count[s[left] - 'A']--;
+                left++;
             }
-            if(str.size() - maxFreq > k){
-                seen[str.front()]--;
-                if(str.front() == maxC) maxFreq--;
-                str.erase(str.begin());
-            }
-            i++;
+
+            best = max(best, right - left + 1);
         }
-        return str.size();
+        return best;
     }
 };
