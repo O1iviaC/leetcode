de4ee1941116edc05d135134e3baf3f84ae5bea1
Olivia Chan <115821929+O1iviaC@users.noreply.github.com>
2026-01-24 22:31:44 -0500

    Time: 0 ms (100%), Space: 20 MB (11.66%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,3 +1,16 @@
+/*
+* we take in nums (an array with all our numbers), and k representing the first k highest occuring numbers within nums
+
+* we create a map where the key of each pair would be the number from nums and the value from the pair represents its number of occurences. This is tracked using the for loop, everytime we see the key (represented by x) we increment its existing value of the pair and if that number never existed it is added to the map and its value is 1
+
+* next we have a 2d array called buckets that is sized one more than the size of nums so that its last index number correctly reflects the size of nums since the maximum number of occurences for a number is the array size and since arrays are 0-indexed we need to make the array size one more than that.
+
+* Now in the 2d array the first dimension represents the total occurences, and using p as a pointer iterating through our map freq, any time we see a key with a specific occurence we add it to the index of that first dimension. the second of the 2d array is a vector of all the keys that have the first dimension's number of occurences
+
+* after iterating through the entire map we now want to return the k most frequently occuring numbers. With the first dimension of buckets representing occurences, this means the higher the array index of the first dimension, the higher the number of occurence so we iterate through this for loop backwards starting from buckets.size() - 1 all the way until we iterated backwards k number of times. At each iteration there is an inner for loop to push back all of the array elements within the vector of the second dimension of the buckets 2d array which holds all of the numbers at each amount of occurences.
+
+* finally we return ans
+*/
 #include <vector>
 #include <unordered_map>
 using namespace std;
