747e0fb06477503215329b1fafb56b4530e4f482
Olivia Chan <115821929+O1iviaC@users.noreply.github.com>
2026-01-25 22:09:48 -0500

    Time: 0 ms (100%), Space: 12.3 MB (28.28%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+#include <unordered_map>
+
+class Solution {
+    unordered_map<Node*, Node*> copy;
+
+    Node* dfs(Node* node) {
+        if (!node) return nullptr;
+
+        // If already cloned, return it (prevents cycles)
+        if (copy.count(node)) return copy[node];
+
+        // Create clone
+        Node* clone = new Node(node->val);
+        copy[node] = clone;
+
+        // Clone neighbors
+        for (Node* nei : node->neighbors) {
+            clone->neighbors.push_back(dfs(nei));
+        }
+
+        return clone;
+    }
+
+public:
+    Node* cloneGraph(Node* node) {
+        return dfs(node);
+    }
+};
