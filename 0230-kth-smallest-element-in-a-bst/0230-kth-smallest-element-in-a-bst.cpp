4b0721ce669d8bde875fc4e1dc0b92d46dcde875
Olivia Chan <115821929+O1iviaC@users.noreply.github.com>
2026-02-03 13:56:22 -0500

    Time: 0 ms (100%), Space: 24.4 MB (40.77%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,4 +1,15 @@
 /**
+How it works:
+This solution takes advantage of the fac thtat BST trees are already sorted where everything on your left is smaller than your root and everything on your right is larger.
+
+I keep a global variable n as a counter to the kth number we want and ans for a return value.
+
+In a helper function called inorder look into the left sides of the tree and when you hit a nullptr return.
+After checking the left child (what would be your smaller element) check the right element.
+Essentially travel as far as you can down the tree finding the smallest element and when you start unravelling the recursion start counting until you reach the kth smallest value. In this case I initialize the value of n with k and I'll decrement every time a recursion unravels until I get to 0 which means I've gotten to my kth smallest number. 
+
+I check the left value first because it's smaller than the right, see if n is 0 to see if I got to the kth smallest yet, and then check the right side. In order you're always going to look at the leftmost side of the tree first and then work up the tree checking left, right, then the root which itself is a left child, then repeat checking the right and then the root.
+
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
